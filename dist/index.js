5f07b67a6ccf037c7dc145b54049056abd2087c3
Ryan Hansen <rphansen91@gmail.com>
2017-03-12 20:25:04 -0700

    Run build

--- dist/index.js
+++ dist/index.js
@@ -61,7 +61,7 @@ var ReduxRTC =
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "//rphansen91.github.io/redux-rtc";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 10);
